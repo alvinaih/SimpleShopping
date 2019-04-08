18fd1fa09e5d6ff665d58723b6649574d2b8c1ab
alvina <alvinahartogh08@gmail.com>
2019-04-08 15:55:16 +0200

    First Commit

---
+++ SimpleShoppingApp/Scripts/App.js
@@ -0,0 +1,40 @@
+﻿var currentList = {};
+
+function createShoppingList()
+{
+    currentList.name = $("#shoppingListName").val();
+    currentList.items = new Array();
+
+    $("#shoppingListTitle").html(currentList.name);
+    $("#shoppingListItems").empty();
+
+    $("#createListDiv").hide();
+    $("#shoppingListDiv").show();
+
+    showShoppingList();
+}
+function showShoppingList() {
+
+    $("#shoppingListTitle").html(currentList.name);
+    $("#shoppingListItems").empty();
+
+    $("#createListDiv").hide();
+    $("#shoppingListDiv").show();
+    $("#newItemName").focus();
+}
+function addItem()
+{
+
+}
+$(document).ready(function ()
+{
+    console.info("Ready!");
+    $("#shoppingListName").focus();
+    $("#shoppingListName").keyup(function (event)
+    {
+        if (event.keyCode == 13)
+        {
+            createShoppingList();
+        }
+    });
+});
